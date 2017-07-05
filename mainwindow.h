2dfe883180f7c301b9f466b9ad56c1f4137dc0fd
sunsc <584156664@qq.com>
2017-07-05 11:18:58 +0800

    first

---
+++ mainwindow.h
@@ -0,0 +1,30 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include<QSerialPort>
+#include<QtSerialPort/QSerialPortInfo>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+    QSerialPort *serial;
+
+private slots:
+    void portscan();
+    void connectport();
+
+};
+
+#endif // MAINWINDOW_H
